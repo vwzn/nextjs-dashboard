cd7c7360dbfda372659d35306aabcaa446547d15
Fauzan <fauzan.abd.1703@gmail.com>
2024-02-29 22:56:48 +0700

    Learn NEXT JS 14

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -7,7 +7,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-gray-100 text-gray-500': status === 'pending',//clsx library
           'bg-green-500 text-white': status === 'paid',
         },
       )}
