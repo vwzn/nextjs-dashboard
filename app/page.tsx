fa23b5351fac99999609ba1030313a7434e69da0
Fauzan <fauzan.abd.1703@gmail.com>
2024-08-06 06:33:54 +0700

    -

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,17 @@ export default function Page() {
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             {/* <div className={styles.shape} /> */}
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Acme.</strong> This website builded by{' '}
+            <a href="https://nextjs.org" className="text-blue-500">
+              Next.js
             </a>
-            , brought to you by Vercel.
+            , TypeScript, and Postgres.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Go to view</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
